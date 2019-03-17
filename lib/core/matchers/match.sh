294cb907d726b482b95525ad47eb0f5ebb8f2110
Koichi Nakashima <koichi@nksm.name>
2019-03-17 18:05:55 +0900

    Fix for shellcheck

--- lib/core/matchers/match.sh
+++ lib/core/matchers/match.sh
@@ -4,6 +4,7 @@ shellspec_syntax 'shellspec_matcher_match'
 
 shellspec_matcher_match() {
   shellspec_matcher__match() {
+    # shellcheck disable=SC2034
     SHELLSPEC_EXPECT=$1
     [ "${SHELLSPEC_SUBJECT+x}" ] || return 1
     eval "case \$SHELLSPEC_SUBJECT in ($1) true;; (*) false;; esac"
