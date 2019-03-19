71ba8b7c4ea7acea3fe40b1d2cae278d56e537c7
Koichi Nakashima <koichi@nksm.name>
2019-03-19 09:17:54 +0900

    Fix function subjects

--- lib/core/subjects/function.sh
+++ lib/core/subjects/function.sh
@@ -8,7 +8,7 @@ shellspec_syntax 'shellspec_subject_function'
 shellspec_subject_function() {
   shellspec_syntax_param count [ $# -ge 1 ] || return 0
 
-  eval "SHELLSPEC_SUBJECT=$($1)"
+  SHELLSPEC_SUBJECT=$($1)
   shift
 
   eval shellspec_syntax_dispatch modifier ${1+'"$@"'}
