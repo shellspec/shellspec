d49cfa2dbc15dc8c978b4adfdcc967c0b41fb208
Koichi Nakashima <koichi@nksm.name>
2019-03-24 18:33:25 +0900

    [ci skip] Update demo

--- contrib/demo/spec/demo_spec.sh
+++ contrib/demo/spec/demo_spec.sh
@@ -2,16 +2,16 @@ Describe 'sample' # Example group block
   Describe 'bc command'
     add() { echo "$1 + $2" | bc; }
 
-    Example 'perform addition' # Example block
+    It 'performs addition' # Example block
       When call add 2 3 # Evaluation
       The output should eq 5  # Expectation
     End
   End
 
   Describe 'implemented by shell function'
-    . ./mylib.sh # add() function defined
+    Include ./mylib.sh # add() function defined
 
-    Example 'perform addition'
+    It 'performs addition'
       When call add 2 3
       The output should eq 5
     End
