0250ebbf320395ada31964567d629814a015a7f0
Koichi Nakashima <koichi@nksm.name>
2019-03-26 19:16:42 +0900

    Add mock stub sample

---
+++ sample/spec/19.mock_stub_spec.sh
@@ -0,0 +1,18 @@
+#shellcheck shell=sh disable=SC2016
+
+Describe 'mock stub sample'
+  unixtime() { date +%s; }
+  get_next_day() { echo $(($(unixtime) + 86400)); }
+
+  Example 'redefine date command'
+    date() { echo 1546268400; }
+    When call get_next_day
+    The stdout should eq 1546354800
+  End
+
+  Example 'use the date command'
+    # Date is not redefined because this is another subshell
+    When call unixtime
+    The stdout should not eq 1546268400
+  End
+End
