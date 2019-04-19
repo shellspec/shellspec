876b00093b303df75fc19da127f4d566a3d6a857
Koichi Nakashima <koichi@nksm.name>
2019-04-19 22:22:56 +0900

    shellspec-example: minor fix

--- libexec/shellspec-examples.sh
+++ libexec/shellspec-examples.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 #shellcheck disable=SC2004
 
+set -eu
+
 # shellcheck source=lib/libexec.sh
 . "${SHELLSPEC_LIB:-./lib}/libexec.sh"
 load parser
 
-i=0
+count=0
 specfile() {
-  while read -r line; do
-    is_example "${line%% *}" && i=$(($i + 1))
+  while read -r line || [ "$line" ]; do
+    if is_example "${line%% *}"; then
+      count=$(($count + 1))
+    fi
   done < "$1"
 }
 find_specfiles specfile "$@"
 
-if [ "$SHELLSPEC_EXAMPLES_LOG" ]; then
-  echo "$i" > "${SHELLSPEC_EXAMPLES_LOG}#"
+if [ "${SHELLSPEC_EXAMPLES_LOG:-}" ]; then
+  echo "$count" > "${SHELLSPEC_EXAMPLES_LOG}#"
   mv "${SHELLSPEC_EXAMPLES_LOG}#" "$SHELLSPEC_EXAMPLES_LOG"
 fi
 
-echo "$i"
+echo "$count"
