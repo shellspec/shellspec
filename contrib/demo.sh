25e33762fa700186b8b975dba64b6862e3a73739
Koichi Nakashima <koichi@nksm.name>
2019-04-24 20:40:13 +0900

    [ci skip] Update demo

--- contrib/demo.sh
+++ contrib/demo.sh
@@ -2,12 +2,19 @@
 
 #ghostplay silent
 # ttyrec -e "ghostplay contrib/demo.sh"
-# seq2gif -l 3000 -h 32 -i ttyrecord -o demo.gif
+# seq2gif -l 3000 -h 32 -w 139 -p win -i ttyrecord -o demo.gif
+GP_HOSTNAME=ubuntu
 #ghostplay end
 
+shellspec
+
+#ghostplay sleep 3
+
 cd contrib/demo
-cat mylib.sh
-cat spec/demo_spec.sh
+#ghostplay sleep 1
+highlight -l -O xterm256 mylib.sh
+#ghostplay sleep 1
+highlight -l -O xterm256 spec/demo_spec.sh
 
 #ghostplay sleep 3
 
