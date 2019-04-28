c9e6a35a4b549cb41a81e7461df4eda046bd1274
Koichi Nakashima <koichi@nksm.name>
2019-04-28 18:43:29 +0900

    Improve contrib/all.sh

--- contrib/all.sh
+++ contrib/all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Run in all supported shells
+# Run in all shells
 
 # This script is for development purposes.
 # It provide as is, do not any support.
@@ -13,14 +13,56 @@
 
 set -eu
 
-[ $# -eq 0 ] && set -- ./shellspec
+: "${TARGET:=sh,ash,dash,bash,zsh,pdksh,ksh,ksh93,mksh,yash,posh,busybox ash}"
 
-for shell in dash bash zsh ksh mksh yash posh 'busybox ash'; do
-  echo "[$shell]"
-  if which "${shell%% *}" > /dev/null; then
-    $shell "$@" &&:
+readlinkf() {
+  p=$1
+  while [ -L "$p" ]; do
+    case $p in (*/*) cd "${p%/*}"; p=${p##*/}; esac
+    l=$(ls -dl "$p") && p=${l#*"$p -> "}
+  done
+  case $p in (*/*) cd "${p%/*}"; p=${p##*/}; esac
+  printf '%s\n' "$PWD/$p"
+}
+
+each_shells() {
+  (
+    callback=$1 IFS=,
+    shift
+    for shell in $TARGET; do
+      shell_path='' real_path=''
+      shell_path=$(which "${shell%% *}") > /dev/null &&:
+      [ -L "${shell_path%% *}" ] && real_path=$(readlinkf "${shell_path%% *}")
+      $callback "$@"
+    done
+  )
+}
+
+info() {
+  info=$shell_path
+  [ "$info" ] && info="$info${real_path:+ -> }$real_path"
+  printf '%8s : %s\n' "${shell%% *}" "${info:-----}"
+}
+
+run() {
+  if [ "$shell_path" ]; then
+    echo "--------------------------------------------------"
+    echo "$shell : $shell_path${real_path:+ -> }$real_path"
+    echo "$shell $@"
+    echo "--------------------------------------------------"
+    eval SH=\$shell $shell "$@"
   else
-    echo "Skip, shell not found"
+    echo "--------------------------------------------------"
+    echo "$shell : Skip, shell not found"
+    echo "--------------------------------------------------"
+    echo
   fi
-  echo
-done
+}
+
+uname -a
+echo "=================================================="
+each_shells info
+echo "=================================================="
+if [ $# -gt 0 ]; then
+  each_shells run "$@"
+fi
