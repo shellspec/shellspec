a4c0402dbd726eef7977603701e5bb0f218b6274
Koichi Nakashima <koichi@nksm.name>
2019-04-29 13:25:45 +0900

    contrib/various_test.sh: add --skip-message queit

--- contrib/various_test.sh
+++ contrib/various_test.sh
@@ -5,10 +5,10 @@ set -eu
 : "${SH:=sh}"
 
 $SH shellspec --banner
-$SH shellspec --no-banner -j 3
-$SH shellspec --no-banner $($SH shellspec --list-specfiles | head -n 5)
-$SH shellspec --no-banner $($SH shellspec --list-examples | head -n 5)
-$SH shellspec --no-banner spec/general_spec.sh:40:60:80:100
+$SH shellspec --no-banner --skip-message queit -j 3
+$SH shellspec --no-banner --skip-message queit $($SH shellspec --list-specfiles | head -n 5)
+$SH shellspec --no-banner --skip-message queit $($SH shellspec --list-examples | head -n 5)
+$SH shellspec --no-banner --skip-message queit spec/general_spec.sh:40:60:80:100
 $SH shellspec --syntax-check
 $SH shellspec --count
 $SH shellspec --task
