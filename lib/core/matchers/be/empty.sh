ca677c7b3d78dd07494954e9857ef5ec819bd9a3
Koichi Nakashima <koichi@nksm.name>
2019-05-25 09:15:52 +0900

    Add empty directory matcher

--- lib/core/matchers/be/empty.sh
+++ lib/core/matchers/be/empty.sh
@@ -1,6 +1,8 @@
 #shellcheck shell=sh
 
 shellspec_syntax 'shellspec_matcher_be_empty_file'
+shellspec_syntax 'shellspec_matcher_be_empty_directory'
+shellspec_syntax_alias 'shellspec_matcher_be_empty_dir' 'shellspec_matcher_be_empty_directory'
 shellspec_syntax_compound 'shellspec_matcher_be_empty'
 
 shellspec_matcher_be_empty_file() {
@@ -15,6 +17,45 @@ shellspec_matcher_be_empty_file() {
 
   shellspec_matcher__failure_message_when_negated() {
     shellspec_putsn "The specified path is empty file"
+    shellspec_putsn "path: $SHELLSPEC_SUBJECT"
+  }
+
+  shellspec_syntax_param count [ $# -eq 0 ] || return 0
+  shellspec_matcher_do_match
+}
+
+shellspec_matcher_be_empty_directory() {
+  shellspec_matcher__match() {
+    [ -d "${SHELLSPEC_SUBJECT:-}" ] || return 1
+
+    # This subshell is used to revert changes directory, $OLDPWD, set, shopt
+    (
+      # set -- "$DIR"/* not working properly in posh 0.10.2
+      cd "$SHELLSPEC_SUBJECT" || return 1
+
+      set +o noglob
+      case $SHELLSPEC_SHELL_TYPE in
+        zsh) setopt NO_NOMATCH ;;
+        bash) { eval shopt -u failglob ||:; } 2>/dev/null ;;
+        posh) set +u ;; # glob does not expand when set -u in posh 0.10.2
+      esac
+      set -- * .*
+
+      while [ $# -gt 0 ]; do
+        case $1 in (.|..) false; esac && [ -e "$1" ] && break
+        shift
+      done
+      [ $# -eq 0 ] &&:
+    )
+  }
+
+  shellspec_matcher__failure_message() {
+    shellspec_putsn "The specified path is not empty directory"
+    shellspec_putsn "path: $SHELLSPEC_SUBJECT"
+  }
+
+  shellspec_matcher__failure_message_when_negated() {
+    shellspec_putsn "The specified path is empty directory"
     shellspec_putsn "path: $SHELLSPEC_SUBJECT"
   }
 
--- spec/core/matchers/be/empty_spec.sh
+++ spec/core/matchers/be/empty_spec.sh
@@ -1,16 +1,17 @@
-#shellcheck shell=sh
+#shellcheck shell=sh disable=SC2016
 
 % FIXTURE: "$SHELLSPEC_SPECDIR/fixture"
+% EMPTYDIR: "$SHELLSPEC_TMPBASE/emptydir"
 
 Describe "core/matchers/be/empty.sh"
   Before set_subject intercept_shellspec_matcher
   subject() { false; }
 
-  Describe 'be empty file matcher'
-    Path empty-file="$FIXTURE/empty"
-    Path not-empty-file="$FIXTURE/file"
-    Path not-exists-file="$FIXTURE/not-exists"
+  Path empty-file="$FIXTURE/empty"
+  Path not-empty-file="$FIXTURE/file"
+  Path not-exists-file="$FIXTURE/not-exists"
 
+  Describe 'be empty file matcher'
     Example 'example'
       The path empty-file should be empty file
       The path not-empty-file should not be empty file
@@ -43,6 +44,100 @@ Describe "core/matchers/be/empty.sh"
 
     It 'outputs error if parameters count is invalid'
       When invoke shellspec_matcher be empty file foo
+      The stderr should equal SYNTAX_ERROR_WRONG_PARAMETER_COUNT
+      The status should be failure
+    End
+  End
+
+  Describe 'be empty directory matcher'
+    Before 'mkdir "$EMPTYDIR"'
+    After 'rmdir "$EMPTYDIR"'
+
+    Example 'example'
+      Path empty-dir="$EMPTYDIR"
+      The path empty-dir should be empty directory
+      The path empty-dir should be empty dir
+    End
+
+    Context 'when path is empty file'
+      subject() { %- "$FIXTURE/empty"; }
+      It 'does not matches'
+        When invoke shellspec_matcher be empty directory
+        The status should be failure
+      End
+    End
+
+    Context 'when path does not exists'
+      subject() { %- "$FIXTURE/not-exists"; }
+      It 'does not matches'
+        When invoke shellspec_matcher be empty directory
+        The status should be failure
+      End
+    End
+
+    Context 'when path empty directory'
+      subject() { %- "$EMPTYDIR"; }
+      It 'matches'
+        When invoke shellspec_matcher be empty directory
+        The status should be success
+      End
+    End
+
+    Context 'when directory contains "file" file'
+      Before 'touch "$EMPTYDIR/file"'
+      After 'rm "$EMPTYDIR/file"'
+      subject() { %- "$EMPTYDIR"; }
+      It 'does not matches'
+        When invoke shellspec_matcher be empty directory
+        The status should be failure
+      End
+    End
+
+    Context 'when directory contains "*" file'
+      Before 'touch "$EMPTYDIR/*"'
+      After 'rm "$EMPTYDIR/*"'
+      subject() { %- "$EMPTYDIR"; }
+      It 'does not matches'
+        When invoke shellspec_matcher be empty directory
+        The status should be failure
+      End
+    End
+
+    Context 'when directory contains ".dot" file'
+      Before 'touch "$EMPTYDIR/.dot"'
+      After 'rm "$EMPTYDIR/.dot"'
+      subject() { %- "$EMPTYDIR"; }
+      It 'does not matches'
+        When invoke shellspec_matcher be empty directory
+        The status should be failure
+      End
+    End
+
+    Context 'when disabled noglob'
+      Before 'touch "$EMPTYDIR/file"'
+      After 'rm "$EMPTYDIR/file"'
+      Before 'set -o noglob'
+
+      subject() { %- "$EMPTYDIR"; }
+      It 'does not matches'
+        When invoke shellspec_matcher be empty directory
+        The status should be failure
+      End
+    End
+
+    Context 'when enabled failglob in bash'
+      Skip if 'is not bash' [ "$SHELLSPEC_SHELL_TYPE" != "bash" ]
+      Before '{ shopt -s failglob ||:; } 2>/dev/null'
+
+      subject() { %- "$EMPTYDIR"; }
+      It 'matches'
+        When invoke shellspec_matcher be empty directory
+        The status should be success
+      End
+    End
+
+    It 'outputs error if parameters count is invalid'
+      When invoke shellspec_matcher be empty directory foo
       The stderr should equal SYNTAX_ERROR_WRONG_PARAMETER_COUNT
       The status should be failure
     End
