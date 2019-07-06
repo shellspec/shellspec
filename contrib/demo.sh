90c5babbddbb7c59cfc13d3b4dec8134bf73ab22
Koichi Nakashima <koichi@nksm.name>
2019-07-06 17:17:23 +0900

    [ci skip] demo.sh: add image for sns

--- contrib/demo.sh
+++ contrib/demo.sh
@@ -3,6 +3,7 @@
 #ghostplay silent
 # ttyrec -e "ghostplay contrib/demo.sh"
 # seq2gif -l 3000 -h 32 -w 139 -p win -i ttyrecord -o demo.gif
+# seq2gif -l 3000 -h 19 -w 83 -p win -i ttyrecord -o sns.gif
 GP_HOSTNAME=ubuntu
 highlight() {
   command highlight -l -O xterm256 --syntax sh
