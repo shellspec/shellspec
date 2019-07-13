6189b25ae5cefd3b4cd48456e6e01b08c7dbed1b
Koichi Nakashima <koichi@nksm.name>
2019-07-13 15:41:13 +0900

    workaround for bosh

--- spec/spec_helper.sh
+++ spec/spec_helper.sh
@@ -87,7 +87,7 @@ shellspec_spec_helper_configure() {
   }
 
   xor_not_support_bug() { # ash 0.3.8
-    ! (eval ': $((0^0))') 2>/dev/null
+    if (eval ': $((0^0))') 2>/dev/null; then false; else true; fi
   }
 
   miscalculate_signed_32bit_int_bug() { # yash 2.30 ans = -2147483648
