8ca77ef86ad9707c066c698b90c5f7137ae7b80b
Koichi Nakashima <koichi@nksm.name>
2019-07-23 08:56:29 +0900

    [ci skip] Support install via npm

--- contrib/make_package_json.sh
+++ contrib/make_package_json.sh
@@ -6,7 +6,7 @@ version() {
 
 files() {
   echo "["
-  files="$(find lib libexec -type f -exec echo "    \"{}\"," \;)"
+  files="$(find bin lib libexec -type f,l -exec echo "    \"{}\"," \;)"
   echo "${files%,}"
   echo "  ]"
 }
@@ -16,8 +16,14 @@ cat<<JSON
   "name": "shellspec",
   "version": "$(version)",
   "description": "BDD style unit testing framework for POSIX compliant shell script",
+  "homepage": "https://shellspec.info",
   "scripts": ["shellspec"],
+  "license": "MIT",
+  "repository": "github:shellspec/shellspec",
   "files": $(files),
-  "install": "make install"
+  "install": "make install",
+  "bin": {
+    "shellspec": "bin/shellspec"
+  }
 }
 JSON
