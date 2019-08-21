37e1f3273d607ee950ccfa77a16c81c0a2cdf17e
Koichi Nakashima <koichi@nksm.name>
2019-08-22 04:12:34 +0900

    Remove unnecessary workaround

--- spec/spec_helper.sh
+++ spec/spec_helper.sh
@@ -11,11 +11,6 @@ shellspec_redefinable shellspec_output_failure_message
 shellspec_redefinable shellspec_output_failure_message_when_negated
 shellspec_redefinable shellspec_on
 shellspec_redefinable shellspec_off
-shellspec_redefinable shellspec_is
-shellspec_redefinable shellspec_syntax_dispatch
-shellspec_redefinable shellspec_callback
-shellspec_redefinable shellspec_shell_option_set_on
-shellspec_redefinable shellspec_shell_option_set_off
 
 # Workaround for busybox-1.1.3
 shellspec_unbuiltin "ps"
