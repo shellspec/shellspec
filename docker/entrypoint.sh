d6386da4ac927e7a50b5d3fb49bfe098a2d89164
Koichi Nakashima <koichi@nksm.name>
2019-08-24 01:10:46 +0900

    Replace fake-sudo to su-exec

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 if [ "$1" = "shellspec" ]; then
-  sudo shellspec --task fixture:stat:prepare
+  (
+    export SUDO_GID=$(id -g user) SUDO_UID=$(id -u user)
+    su-exec root shellspec --task fixture:stat:prepare
+  )
 fi
-
-[ "$(id -u)" -eq 0 ] && set -- su user "$@"
 
 "$@"
