117b5dcf9e38ae1a9817f0104346453aaea389ad
Koichi Nakashima <koichi@nksm.name>
2020-01-26 18:58:10 +0900

    [ci skip] Remove .env

--- contrib/build.sh
+++ contrib/build.sh
@@ -2,6 +2,19 @@
 
 set -eu
 
+if [ -e .env ]; then
+  default_compose_project_name=shellspec
+  while read -r line; do
+    case $line in (COMPOSE_PROJECT_NAME=*)
+      default_compose_project_name=''
+    esac
+  done < .env
+  if [ "$default_compose_project_name" ]; then
+    : "${COMPOSE_PROJECT_NAME:=$default_compose_project_name}"
+    export COMPOSE_PROJECT_NAME
+  fi
+fi
+
 for dockerfile; do
   export DOCKERFILE_PATH=${dockerfile#.dockerhub/}
 
