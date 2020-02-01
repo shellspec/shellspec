8b32dcc31bbd888882ebccedc64415bdf13f9063
Koichi Nakashima <koichi@nksm.name>
2020-02-01 18:28:46 +0900

    Deprecated --kcov-common-options

--- lib/libexec/kcov-executor.sh
+++ lib/libexec/kcov-executor.sh
@@ -13,10 +13,12 @@ executor() {
   translator --functrace --fd=537 "$@" > "$SHELLSPEC_KCOV_IN_FILE"
 
   #shellcheck disable=SC2039,SC2086
-  "$SHELLSPEC_KCOV_PATH" --bash-method=DEBUG \
+  "$SHELLSPEC_KCOV_PATH" \
+    $SHELLSPEC_KCOV_COMMON_OPTS \
+    $SHELLSPEC_KCOV_OPTS \
+    --bash-method=DEBUG \
     --bash-parser="$SHELLSPEC_SHELL" \
-    --bash-parse-files-in-dir="$SHELLSPEC_PROJECT_ROOT" \
-    $SHELLSPEC_KCOV_COMMON_OPTS $SHELLSPEC_KCOV_OPTS \
+    --bash-parse-files-in-dir=. \
     "$SHELLSPEC_COVERAGEDIR" "$SHELLSPEC_KCOV_IN_FILE" 537>&1
 
   # Fix symbolic link to relative path
