3b3611a3da8ca6dc6e2b5769ac99b91e9deabcf2
Koichi Nakashima <koichi@nksm.name>
2020-02-09 15:30:11 +0900

    Fixes test in docker on linux

--- dockerfiles/.shellspec-entrypoint.sh
+++ dockerfiles/.shellspec-entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = "shellspec" ]; then
   (
-    export SUDO_GID=$(id -g user) SUDO_UID=$(id -u user)
+    export SUDO_GID=$(id -g) SUDO_UID=$(id -u)
     su-exec root shellspec --task fixture:stat:prepare
   )
 fi
