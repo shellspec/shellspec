9d98374f0206b124f0913b16d43456dff6b9a3b4
Koichi Nakashima <koichi@nksm.name>
2020-04-26 19:52:23 +0900

    Add tests for shellspec-load-env.sh

---
+++ spec/shellspec-load-env_spec.sh
@@ -0,0 +1,14 @@
+#shellcheck shell=sh disable=SC2016
+
+% FIXTURE: "$SHELLSPEC_SPECDIR/fixture"
+
+Describe "shellspec-load-env.sh"
+  BeforeRun 'export SHELLSPEC_ENV_FROM=$FIXTURE/env-script.sh'
+  BeforeRun 'export SHELLSPEC_LIBEXEC=$FIXTURE'
+  BeforeRun 'export SHELLSPEC_MODE=dummy-mode'
+  It 'loads env file'
+    When run script ./libexec/shellspec-load-env.sh
+    The line 1 of stdout should eq "env-script"
+    The line 2 of stdout should eq "dummy mode"
+  End
+End
