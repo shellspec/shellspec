2679d6e7eeead962c1855630d51925a08c0f8740
Koichi Nakashima <koichi@nksm.name>
2020-05-09 06:32:26 +0900

    [ci skip] contrib/various_test: Skip some tests for reduce test time

--- contrib/various_test.sh
+++ contrib/various_test.sh
@@ -15,7 +15,7 @@ shellspec --no-banner --skip-message quiet -j 3
 shellspec --no-banner --skip-message quiet $(shellspec --list specfiles | head -n 5)
 shellspec --no-banner --skip-message quiet $(shellspec --list examples:lineno | head -n 5)
 shellspec --no-banner --skip-message quiet spec/general_spec.sh:40:60:80:100
-shellspec --no-banner --skip-message quiet --profile
+shellspec --no-banner --skip-message quiet spec/libexec --profile
 shellspec --syntax-check
 shellspec --count
 shellspec --task
