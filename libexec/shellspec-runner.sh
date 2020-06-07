46b1fccc724934b52b8642b483ccc3dc5c95f509
Koichi Nakashima <koichi@nksm.name>
2020-06-08 00:06:18 +0900

    Output to SHELLSPEC_OUTPUT_FD instead of stdout

--- libexec/shellspec-runner.sh
+++ libexec/shellspec-runner.sh
@@ -136,7 +136,7 @@ fi
 # and capture stderr both of the runner and the reporter
 # and the stderr streams to error hander
 # and also handle both exit status. As a result of
-( ( ( ( set -e; executor "$@"; echo $? >&5 ) \
+( ( ( ( set -e; executor "$@" 9>&1 >&8; echo $? >&5 ) \
   | reporter "$@" >&3; echo $? >&5 ) 2>&1 \
   | error_handler >&4; echo $? >&5 ) 5>&1 \
   | (
@@ -153,7 +153,7 @@ fi
       esac
       set_exit_status "${xs:-1}"
     )
-) 3>&1 4>&2 &&:
+) 3>&1 4>&2 8>&1 &&:
 exit_status=$?
 
 case $exit_status in
