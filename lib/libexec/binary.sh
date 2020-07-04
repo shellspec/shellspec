a71854bdf792f93f3834f047fed77b8910c7d3b4
Koichi Nakashima <koichi@nksm.name>
2020-07-04 11:03:52 +0900

    od_command: Refactor

--- lib/libexec/binary.sh
+++ lib/libexec/binary.sh
@@ -2,15 +2,11 @@
 
 # busybox 1.1.3: `-A n`, `-t o1` not supported
 # busybox 1.10.2: `od -b` not working properly
-if od -t o1 -v /dev/null >/dev/null 2>&1; then
-  od_command() { od -t o1 -v; }
-else
-  if [ $? -eq 127 ] && hexdump /dev/null >/dev/null 2>&1; then
-    od_command() { hexdump -b -v; }
-  else
-    od_command() { od -b -v; }
-  fi
-fi
+od_command() {
+  od -t o1 -v 2>/dev/null && return 0
+  [ $? -eq 127 ] && hexdump -b -v 2>/dev/null && return 0
+  od -b -v
+}
 
 octal_dump() {
   od_command | (
