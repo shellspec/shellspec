eff5ae0aabe35614328b87f7a46bc47d1408187c
Koichi Nakashima <koichi@nksm.name>
2020-07-08 20:44:42 +0900

    Update support command template

--- libexec/shellspec-gen-bin.sh
+++ libexec/shellspec-gen-bin.sh
@@ -6,14 +6,9 @@ test || __() { :; }
 
 # shellcheck disable=SC2016
 generate() {
-  echo '#!/bin/sh -e'
-  echo 'if [ "${SHELLSPEC_PATH_IS_READONLY:-}" ]; then'
-  echo '  typeset +x PATH'
-  echo '  exec "$SHELLSPEC_ENV" PATH="$PATH" SHELLSPEC_PATH_IS_READONLY="" "$0" "$@"'
-  echo 'fi'
-  echo 'PATH="${SHELLSPEC_PATH:?}"'
-  echo 'export PATH'
-  echo "$1" '"$@"'
+  echo "#!/bin/sh -e"
+  echo ". \"\$SHELLSPEC_SUPPORT_BIN\""
+  echo "invoke $1 \"\$@\""
 }
 
 __ main __
