a0988ee910e6d9bc52e81505ebb113ac5381af3a
Koichi Nakashima <koichi@nksm.name>
2020-09-14 22:05:29 +0900

    TAP formatter: Added error details

--- lib/libexec/reporter/tap_formatter.sh
+++ lib/libexec/reporter/tap_formatter.sh
@@ -2,8 +2,10 @@
 
 : "${count_examples:-} ${aborted:-} ${example_count:-} ${reason:-}"
 : "${field_color:-} ${field_type:-} ${field_note:-} ${field_fail:-}"
-: "${field_description:-} ${field_message:-}"
+: "${field_description:-} ${field_message:-} ${field_tag:-} ${field_lineno:-}"
+: "${field_specfile:-} ${field_evaluation:-} ${field_failure_message:-}"
 
+tap_failures=''
 create_buffers tap
 
 tap_initialize() {
@@ -15,21 +17,39 @@ tap_begin() {
 }
 
 tap_each() {
-  case $field_type in (result)
-    case $field_note in
-      PENDING) _result="not ok" _note="TODO" ;;
-      FIXED  ) _result="ok" _note="TODO" ;;
-      SKIPPED) _result="ok" _note="SKIP" ;;
-      *) _result="${field_fail:+not }ok" _note=$field_note ;;
-    esac
-
-    tap '=' "${field_color}${_result} ${example_count}${RESET}"
-    tap '+=' " - $(field_description)"
-    if [ "${_note}${reason}" ]; then
-      _comment="${_note:+ $MAGENTA}${_note}${reason:+ $WHITE}${reason#\#\ }"
-      tap '+=' " ${WHITE}#${_comment}${RESET}"
-    fi
-    tap '+=' "${LF}"
+  _color=$field_color
+  case $field_type in
+    statement)
+      case $field_tag in (bad | warn)
+        _failure_line="in specfile $field_specfile, line $field_lineno"
+        [ "${field_note}" ] && _failure_line="$_failure_line, $field_note"
+        tap_failures="${tap_failures}${_color}($_failure_line)${LF}"
+        tap_failures="${tap_failures}${_color}${field_evaluation}${LF}"
+        tap_failures="${tap_failures}${_color}${field_message}${LF}"
+        tap_failures="${tap_failures}${_color}${field_failure_message}${LF}"
+      esac ;;
+    result)
+      case $field_note in
+        PENDING) _result="not ok" _note="TODO" ;;
+        FIXED  ) _result="ok" _note="TODO" ;;
+        SKIPPED) _result="ok" _note="SKIP" ;;
+        *) _result="${field_fail:+not }ok" _note=$field_note ;;
+      esac
+      tap '=' "${_color}${_result} ${example_count}${RESET}"
+      tap '+=' " - $(field_description)"
+      if [ "${_note}${reason}" ]; then
+        _comment="${_note:+ $MAGENTA}${_note}${reason:+ $WHITE}${reason#\#\ }"
+        tap '+=' " ${WHITE}#${_comment}${RESET}"
+      fi
+      tap '+=' "${LF}"
+      if [ "$tap_failures" ]; then
+        set -- "$tap_failures" ""
+        while [ "$1" ]; do
+          set -- "${1#*"${LF}"}" "$2${_color}# ${1%%"$LF"*}${RESET}${LF}"
+        done
+        tap '+=' "$2"
+        tap_failures=''
+      fi
   esac
 }
 
