4100b6fe80889f70eb76c26080dd9b3b00a9ace8
Koichi Nakashima <koichi@nksm.name>
2020-09-21 20:16:09 +0900

    various_test.sh: Add generation to report files

--- contrib/various_test.sh
+++ contrib/various_test.sh
@@ -10,7 +10,7 @@ shellspec() {
   "$@"
 }
 
-shellspec --banner
+shellspec --banner --output progress --output documentation --output tap --output junit --output failures
 shellspec --no-banner --skip-message quiet -j 3
 shellspec --no-banner --skip-message quiet $(shellspec --list specfiles | head -n 5)
 shellspec --no-banner --skip-message quiet $(shellspec --list examples:lineno | head -n 5)
