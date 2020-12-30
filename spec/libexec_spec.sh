321e425953abf63de3dbff955e90eea1370f615b
Koichi Nakashima <koichi@nksm.name>
2020-12-30 15:45:41 +0900

    Fix for busybox-w32

--- spec/libexec_spec.sh
+++ spec/libexec_spec.sh
@@ -209,7 +209,7 @@ Describe 'libexec.sh'
   End
 
   Describe "timeout()"
-    sleep() { "$SHELLSPEC_SLEEP" 0; }
+    sleep() { "$SHELLSPEC_ENV" sleep 0; }
     _timeout() { { ( sleep 1 ) & timeout 0 $!; } 2>/dev/null; }
     It "stops when timed out"
       When call _timeout 1
