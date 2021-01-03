10305245e7a2f114528fc7f6895e2ffc0e035d1f
Koichi Nakashima <koichi@nksm.name>
2021-01-03 16:02:47 +0900

    Update example

--- examples/spec/spec_helper.sh
+++ examples/spec/spec_helper.sh
@@ -2,12 +2,6 @@
 
 # set -eu
 
-# shellspec_redefinable function_name
-#
-#  shellspec_redefinable is workaround for ksh (Version AJM 93u+ 2012-08-01)
-#  ksh can not redefine existing function in some cases inside of sub shell.
-#  If you have trouble in redefine function on ksh, try using shellspec_redefinable.
-
-shellspec_spec_helper_configure() {
-  shellspec_import 'support/custom_matcher'
+spec_helper_configure() {
+  import 'support/custom_matcher'
 }
