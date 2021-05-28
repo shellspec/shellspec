eb3609680b2512392627c16e0b352027bb2e7ce8
Robert Gurol <robert.gurol@signavio.com>
2021-05-28 17:01:43 +0200

    use "be existent" as default

--- spec/install_spec.sh
+++ spec/install_spec.sh
@@ -130,7 +130,7 @@ Describe "./install.sh"
 
       It 'fetchs archive'
         When call fetch "http://repo.test/b3d5591.tar.gz" "$TMPBASE/curl"
-        The file "$TMPBASE/curl/README.md" should be exist
+        The file "$TMPBASE/curl/README.md" should be existent
         The stderr should be defined # ignore stderr
       End
     End
@@ -146,7 +146,7 @@ Describe "./install.sh"
 
       It 'fetchs archive'
         When call fetch "http://repo.test/b3d5591.tar.gz" "$TMPBASE/wget"
-        The file "$TMPBASE/wget/README.md" should be exist
+        The file "$TMPBASE/wget/README.md" should be existent
         The stderr should be defined # ignore stderr
       End
     End
@@ -158,7 +158,7 @@ Describe "./install.sh"
 
       It 'does not create directory'
         When call fetch "http://repo.test/b3d5591.tar.gz" "$TMPBASE/error"
-        The directory "$TMPBASE/error" should not be exist
+        The directory "$TMPBASE/error" should not be existent
         The status should be failure
       End
     End
