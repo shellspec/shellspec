3f0cd9b3e1db112ae4234aedf6c2b3994c1af7c2
Robert Gurol <robert.gurol@signavio.com>
2021-07-31 15:01:34 +0200

    fix failure message param

--- lib/core/matchers/exist.sh
+++ lib/core/matchers/exist.sh
@@ -7,9 +7,9 @@ shellspec_matcher_exist() {
   }
 
   shellspec_syntax_failure_message + \
-    'expected $2 to exist'
+    'expected $1 to exist'
   shellspec_syntax_failure_message - \
-    'did not expect $2 to exist'
+    'did not expect $1 to exist'
 
   shellspec_syntax_param count [ $# -eq 0 ] || return 0
   shellspec_matcher_do_match "$@"
