72d16705dcdb730e83d04cbf8403cb7186c71f85
Robert Gurol <robert.gurol@signavio.com>
2021-07-31 14:46:59 +0200

    adjust failure message

--- lib/core/matchers/exist.sh
+++ lib/core/matchers/exist.sh
@@ -7,9 +7,9 @@ shellspec_matcher_exist() {
   }
 
   shellspec_syntax_failure_message + \
-    'expected file $2 to exist'
+    'expected $2 to exist'
   shellspec_syntax_failure_message - \
-    'did not expect file $2 to exist'
+    'did not expect $2 to exist'
 
   shellspec_syntax_param count [ $# -eq 0 ] || return 0
   shellspec_matcher_do_match "$@"
