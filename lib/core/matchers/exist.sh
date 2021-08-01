5d58c34653022d38ad16f7d0f8929fdece5bd35d
Robert Gurol <robert.gurol@signavio.com>
2021-08-01 13:19:11 +0200

    fix for `posh`

--- lib/core/matchers/exist.sh
+++ lib/core/matchers/exist.sh
@@ -11,5 +11,5 @@ shellspec_matcher_exist() {
     'did not expect $1 to exist'
 
   shellspec_syntax_param count [ $# -eq 0 ] || return 0
-  shellspec_matcher_do_match "$@"
+  shellspec_matcher_do_match
 }
