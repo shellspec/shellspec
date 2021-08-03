c7a4fa1ae321af4259c5585289c65a8f78d754e8
Robert Gurol <robert.gurol@signavio.com>
2021-08-03 08:51:25 +0200

    ISSUE-179 make new version the new default

--- lib/core/matchers/have/stat.sh
+++ lib/core/matchers/have/stat.sh
@@ -1,11 +1,13 @@
 #shellcheck shell=sh disable=SC2016
 
-shellspec_syntax 'shellspec_matcher_has_setgid'
-shellspec_syntax_alias 'shellspec_matcher_have_setgid' 'shellspec_matcher_has_setgid'
-shellspec_syntax 'shellspec_matcher_has_setuid'
-shellspec_syntax_alias 'shellspec_matcher_have_setuid' 'shellspec_matcher_has_setuid'
+shellspec_syntax 'shellspec_matcher_have_setgid'
+shellspec_syntax 'shellspec_matcher_have_setuid'
 
-shellspec_matcher_has_setgid() {
+# deprecated
+shellspec_syntax_alias 'shellspec_matcher_has_setgid' 'shellspec_matcher_have_setgid'
+shellspec_syntax_alias 'shellspec_matcher_has_setuid' 'shellspec_matcher_have_setuid'
+
+shellspec_matcher_have_setgid() {
   shellspec_matcher__match() {
     [ -g "${SHELLSPEC_SUBJECT:-}" ]
   }
@@ -18,12 +20,12 @@ shellspec_matcher_has_setgid() {
   shellspec_matcher_do_match
 }
 
-shellspec_matcher_has_setuid() {
+shellspec_matcher_have_setuid() {
   shellspec_matcher__match() {
     [ -u "${SHELLSPEC_SUBJECT:-}" ]
   }
 
-  shellspec_syntax_failure_message + 'The $1 does not have setuid flag'
+  shellspec_syntax_failure_message + 'The $1 does not have the setuid flag'
   shellspec_syntax_failure_message - 'The $1 has the setuid flag'
 
   [ "${1:-}" = "flag" ] && shift
