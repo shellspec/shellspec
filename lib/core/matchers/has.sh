31b63f68f3320c75219d34f1b340976ea2c28740
Robert Gurol <robert.gurol@signavio.com>
2021-08-03 07:20:07 +0200

    ISSUE-179 allow "should have sid" / "should have gid" in matchers

--- lib/core/matchers/has.sh
+++ lib/core/matchers/has.sh
@@ -1,5 +1,6 @@
 #shellcheck shell=sh
 
 shellspec_syntax_chain 'shellspec_matcher_has'
+shellspec_syntax_chain 'shellspec_matcher_have'
 
 shellspec_import 'core/matchers/has/stat'
--- lib/core/matchers/has/stat.sh
+++ lib/core/matchers/has/stat.sh
@@ -1,15 +1,17 @@
 #shellcheck shell=sh disable=SC2016
 
 shellspec_syntax 'shellspec_matcher_has_setgid'
+shellspec_syntax_alias 'shellspec_matcher_have_setgid' 'shellspec_matcher_has_setgid'
 shellspec_syntax 'shellspec_matcher_has_setuid'
+shellspec_syntax_alias 'shellspec_matcher_have_setuid' 'shellspec_matcher_has_setuid'
 
 shellspec_matcher_has_setgid() {
   shellspec_matcher__match() {
     [ -g "${SHELLSPEC_SUBJECT:-}" ]
   }
 
-  shellspec_syntax_failure_message + 'The $1 not have setgid flag'
-  shellspec_syntax_failure_message - 'The $1 has setgid flag'
+  shellspec_syntax_failure_message + 'The $1 does not have the setgid flag'
+  shellspec_syntax_failure_message - 'The $1 has the setgid flag'
 
   [ "${1:-}" = "flag" ] && shift
   shellspec_syntax_param count [ $# -eq 0 ] || return 0
@@ -21,8 +23,8 @@ shellspec_matcher_has_setuid() {
     [ -u "${SHELLSPEC_SUBJECT:-}" ]
   }
 
-  shellspec_syntax_failure_message + 'The $1 not have setuid flag'
-  shellspec_syntax_failure_message - 'The $1 has setuid flag'
+  shellspec_syntax_failure_message + 'The $1 does not have setuid flag'
+  shellspec_syntax_failure_message - 'The $1 has the setuid flag'
 
   [ "${1:-}" = "flag" ] && shift
   shellspec_syntax_param count [ $# -eq 0 ] || return 0
